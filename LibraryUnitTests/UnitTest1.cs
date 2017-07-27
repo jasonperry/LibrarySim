e748fc72dcc940a995b89aa36561da7dcbd6c583
Jason Perry <perryjn@lewisu.edu>
2017-07-27 11:33:50 -0500

    initial unit tests project

---
+++ LibraryUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibraryCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMakeBookshelf()
+        {
+            var bookcase1 = new Bookcase<DeweyCallNumber>(4, 1000, 300, 400);
+        }
+    }
+}
