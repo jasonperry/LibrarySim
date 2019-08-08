33bf970d525ebcc7d10dee39d1c60e000cbc00f4
Jason Perry <sh.jperry@gmail.com>
2019-08-08 13:56:42 -0500

    book collection sort, load, save

--- SubjectGraph/BookTypes.fs
+++ SubjectGraph/BookTypes.fs
@@ -2,6 +2,10 @@
 module BookTypes
 
 open System // Uri
+open System.IO
+open System.Runtime.Serialization.Formatters.Binary // TODO: maybe replace with FsPickler.
+
+open Common
 open CallNumber
 
 /// A record about a subject, not dependent on SubjectGraph.
@@ -42,4 +46,33 @@ module BookRecord =
                     else 
                         info :: (update rest)
         { record with Subjects = update record.Subjects }
- 
+    let getLCCNString record = 
+        match record.LCCallNum with 
+        | Some cn -> LCCN.toString cn
+        | None -> record.LCLetters |? "--none--"
+
+let loadBooks (filename: string) = 
+    let formatter = BinaryFormatter()
+    let stream = new FileStream(filename, FileMode.Open)
+    let bookSeq = formatter.Deserialize stream :?> seq<BookRecord>
+    stream.Close()
+    bookSeq
+
+let saveBooks (books: seq<BookRecord>) filename = 
+    let formatter = BinaryFormatter()
+    let stream = new FileStream(filename, FileMode.Create)
+    formatter.Serialize(stream, books)
+    stream.Close()
+// should sortBooks be here or somewhere else?
+open System.Collections.Generic
+
+/// sort a list of books by call letters only (for Gutenberg).
+let sortBooksByCallLetters (books: seq<BookRecord>) = 
+    books
+    |> Seq.sortWith (fun br1 br2 -> 
+        match (br1.LCLetters, br2.LCLetters) with 
+        | (None, None) -> 0
+        | (None, Some _) -> 1 // Nones go at end
+        | (Some _, None) -> -1
+        | (Some cl1, Some cl2) -> String.Compare(cl1, cl2)
+    )
